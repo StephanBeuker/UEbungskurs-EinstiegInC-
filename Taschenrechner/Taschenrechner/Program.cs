d126d809c6b67b5c53f82b67a43bc126a9220a9a
Stephan Beuker <s.beuker@gmx.de>
2020-08-30 19:40:44 +0200

    Kapselung der Berechnung und der Ausgabe in eigenen Methoden

--- Taschenrechner/Taschenrechner/Program.cs
+++ Taschenrechner/Taschenrechner/Program.cs
@@ -14,38 +14,63 @@ namespace Taschenrechner
             // EingabeMethode
             string ersteEingabe = HoleBenutzerEingabe("Bitte gib die erste Zahl ein: ");
             string zweiteEingabe = HoleBenutzerEingabe("Bitte gib die zweite Zahl ein: ");
-            string operation = HoleBenutzerEingabe("Bitte gib die auszuführende Operation ein (+ oder -):");
-
+            string operation = HoleBenutzerEingabe("Bitte gib die auszuführende Operation ein: \n\t\t+ | - | * | /");
+            
             // Wandel Text in Gleitkommazahl
             // TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist.
             double ersteEinagabeAlsZahl = Convert.ToDouble(ersteEingabe);
             double zweiteEingabeAlsZahl = Convert.ToDouble(zweiteEingabe);
 
             // Berechnung
-            double result = 0;
-            switch (operation)
-            {
-                case "+":
-                    result = Addiere(ersteEinagabeAlsZahl, zweiteEingabeAlsZahl);
-                    Console.WriteLine("Die Summe ist: {0}", result);
-                    break;
-                case "-":
-                    result = Subtrahiere(ersteEinagabeAlsZahl, zweiteEingabeAlsZahl);
-                    Console.WriteLine("Die Differenz ist: {0}", result);
-                    break;
-                case "*":
-                case "/":
-                    Console.WriteLine("Diese Operation befindet sich zur Zeit noch in Bearbeitung. Bitte haben Sie noch etwas Geduld. Vielen Dank.");
-                    break;
-                default:
-                    Console.WriteLine("falsche Eingabe.");
-                    break;
-            }
+            double resultat = Berechnung(ersteEinagabeAlsZahl, zweiteEingabeAlsZahl, operation);
 
             // Ausgabe
+            GibResultatAus(resultat, operation);
             HoleBenutzerEingabe("\n\t >>> Zum beenden bitte return drücken <<<\n");
         }    
         // Methoden
+         static double Berechnung(double a, double b, string _operation)
+        {
+            double resultat = 0;
+            switch (_operation)
+            {
+                case "+": 
+                    resultat = Addiere(a, b);
+                    break;
+                case "-":
+                    resultat = Subtrahiere(a, b);
+                    break;
+                case "*":
+                    resultat = Multipliziere(a, b);
+                    break;
+                case "/":
+                    resultat = Dividiere(a, b);
+                    break;
+            }
+            return resultat;
+        }
+        static void GibResultatAus(double resultat, string _operation)
+        {
+            switch (_operation)
+            {
+                case "+":
+                    Console.WriteLine("Die Sumnme ist: {0}", resultat);
+                    break;
+                case "-":
+                    Console.WriteLine("Die Differenz ist: {0}", resultat);
+                    break;
+                case "*":
+                    Console.WriteLine("Das Produkt ist: {0}", resultat);
+                    break;
+                case "/":
+                    Console.WriteLine("Der Quotient ist: {0}", resultat);
+                    break;
+                default:
+                    Console.WriteLine("Ungütige Eingabe");
+                    break;
+            }
+            
+        }
         static string HoleBenutzerEingabe(string ausgabetext)
         {
             Console.WriteLine(ausgabetext);
@@ -59,6 +84,14 @@ namespace Taschenrechner
         static double Addiere(double a, double b)
         {
             return a + b;
+        }
+        static double Multipliziere(double a, double b)
+        {
+            return a * b;
+        }
+        static double Dividiere(double a, double b)
+        {
+            return a / b;
         }
 
     }
