d4f4da6c5ecae7ec7012b9d03b386517d3f2e8cc
Stephan Beuker <s.beuker@gmx.de>
2020-08-31 20:11:48 +0200

    jau

---
+++ Taschenrechner/Taschenrechner/ConsoleView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    class ConsoleView
+    {
+        private RechnerModel model;
+        public ConsoleView(RechnerModel model)
+        {
+            this.model = model;
+        }
+        public string HoleBenutzerEingabe(string ausgabetext)
+        {
+            Console.WriteLine(ausgabetext);
+            return Console.ReadLine();
+        }
+        public void GibResultatAus(string _operation)
+        {
+            switch (_operation)
+            {
+                case "+":
+                    Console.WriteLine("Die Sumnme ist: {0}", model.Resultat);
+                    break;
+                case "-":
+                    Console.WriteLine("Die Differenz ist: {0}", model.Resultat);
+                    break;
+                case "*":
+                    Console.WriteLine("Das Produkt ist: {0}", model.Resultat);
+                    break;
+                case "/":
+                    Console.WriteLine("Der Quotient ist: {0}", model.Resultat);
+                    break;
+                default:
+                    Console.WriteLine("Ungütige Eingabe");
+                    break;
+            }
+
+        }
+
+    }
+}
--- Taschenrechner/Taschenrechner/Program.cs
+++ Taschenrechner/Taschenrechner/Program.cs
@@ -11,10 +11,12 @@ namespace Taschenrechner
     {
         static void Main(string[] args)
         {
+            RechnerModel model = new RechnerModel();
+            ConsoleView view = new ConsoleView(model);
             // EingabeMethode
-            string ersteEingabe = HoleBenutzerEingabe("Bitte gib die erste Zahl ein: ");
-            string zweiteEingabe = HoleBenutzerEingabe("Bitte gib die zweite Zahl ein: ");
-            string operation = HoleBenutzerEingabe("Bitte gib die auszuführende Operation ein: \n\t\t+ | - | * | /");
+            string ersteEingabe = view.HoleBenutzerEingabe("Bitte gib die erste Zahl ein: ");
+            string zweiteEingabe = view.HoleBenutzerEingabe("Bitte gib die zweite Zahl ein: ");
+            string operation = view.HoleBenutzerEingabe("Bitte gib die auszuführende Operation ein: \n\t\t+ | - | * | /");
             
             // Wandel Text in Gleitkommazahl
             // TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist.
@@ -22,42 +24,14 @@ namespace Taschenrechner
             double zweiteEingabeAlsZahl = Convert.ToDouble(zweiteEingabe);
 
             // Berechnung
-            RechnerModel model = new RechnerModel();
             model.Berechne(ersteEinagabeAlsZahl, zweiteEingabeAlsZahl, operation);
 
             // Ausgabe
-            GibResultatAus(model.Resultat, operation);
-            HoleBenutzerEingabe("\n\t >>> Zum beenden bitte return drücken <<<\n");
+            view.GibResultatAus(operation);
+            view.HoleBenutzerEingabe("\n\t >>> Zum beenden bitte return drücken <<<\n");
         }
-        // Methoden
-        static void GibResultatAus(double resultat, string _operation)
-        {
-            switch (_operation)
-            {
-                case "+":
-                    Console.WriteLine("Die Sumnme ist: {0}", resultat);
-                    break;
-                case "-":
-                    Console.WriteLine("Die Differenz ist: {0}", resultat);
-                    break;
-                case "*":
-                    Console.WriteLine("Das Produkt ist: {0}", resultat);
-                    break;
-                case "/":
-                    Console.WriteLine("Der Quotient ist: {0}", resultat);
-                    break;
-                default:
-                    Console.WriteLine("Ungütige Eingabe");
-                    break;
-            }
-
-        }
-        static string HoleBenutzerEingabe(string ausgabetext)
-        {
-            Console.WriteLine(ausgabetext);
-            string eingabe = Console.ReadLine();
-            return eingabe;
-        }
+       
+       
 
     }
 }
